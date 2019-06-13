7c9246c679e40b8ab3e4232610dfc9f3a0702e7c
Jorlost19 <jorlost19@gmail.com>
2019-06-13 18:42:42 -0500

    added response for base url

--- api/index.js
+++ api/index.js
@@ -8,7 +8,8 @@ const server = express();
 
 server.use(helmet(), cors(), express.json({ limit: '50mb', extended: true, parameterLimit: 50000 }))
 server.use('/users', userRouter);
-
-
+server.get('/', (req, res) => {
+    res.status(200).send('The API is running duh!')
+})
 
 module.exports = server;
