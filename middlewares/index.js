4f6a19bf61378ad7852a7ecc8f8250ee9fab096f
Jorlost19 <jorlost19@gmail.com>
2019-06-13 16:48:00 -0500

    added middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET_KEY;
+
+const isLoggedIn = (req, res, next) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization;
+    jwt.verify(token, secret, (err, decodedToken) => {
+      err ? res.status(401).json({ message: 'Invalid token!' }) : next();
+    });
+  } else {
+    res.status(403).json({ error: 'FORBIDDEN!!!' });
+  }
+};
+
+const isValidEmail = (req, res, next) => {
+  req.body.email.match(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  )
+    ? next()
+    : res.status(400).json({
+        message: 'Invalid email account, please verify your data and try again!'
+      });
+};
+
+const isValidPassword = (req, res, next) => {
+  req.body.password.match(
+    /^(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!?<>-_]).*$/
+  )
+    ? next()
+    : res.status(400).json({
+        message:
+          'Password must be at least 8 characters long and contain one lowercase letter, one uppercase letter, one number and one special character!'
+      });
+};
+
+module.exports = { isLoggedIn, isAdmin, isValidEmail, isValidPassword };
