0c270122d350a425fd6e9e3ce76d5fcb6f56321c
Jorlost19 <jorlost19@gmail.com>
2019-06-13 16:20:33 -0500

    finished endpoint for retrieving all users

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -10,6 +10,15 @@ router.post('/register', (req, res) => {
             throw err;
         })
 
+}).get('/', (req, res) => {
+    User.find()
+        .then(users => res.status(200).json(users.map(user => { return {...user._doc, _id: user.id}})))
+        .catch(err => {
+            console.error(err);
+            throw err
+        })
 })
+
+
 
 module.exports = router;
